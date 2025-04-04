14ebed718ea4fd48fead4d620603e2184658bf30
briancosentino <cosentinobrian@gmail.com>
2025-04-04 12:24:30 +0200

    Add language flags to MovieCard component; implement findCountry function for language display

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,6 +5,22 @@ const MovieCard = () => {
     const { movie } = useGlobal()
     console.log(movie);
 
+    const languages = [
+        { language: "it", img: "https://flagcdn.com/w40/it.png" }, // Italiano 🇮🇹
+        { language: "en", img: "https://flagcdn.com/w40/gb.png" }, // Inglese 🇬🇧
+        { language: "fr", img: "https://flagcdn.com/w40/fr.png" }, // Francese 🇫🇷
+        { language: "de", img: "https://flagcdn.com/w40/de.png" }  // Tedesco 🇩🇪
+    ];
+
+    const findCountry = (lang) => {
+        const thisCountry = languages.find(language => lang === language.language)
+
+        return thisCountry
+
+
+    }
+    console.log(findCountry('en'));
+
     return (
         <div>
             {
@@ -12,7 +28,7 @@ const MovieCard = () => {
                     <ul key={movie.id}>
                         <li>{movie.title}</li>
                         <li>{movie.original_title}</li>
-                        <li>{movie.original_language}</li>
+                        <li>{findCountry(movie.original_language) ? <img src={findCountry(movie.original_language)?.img} alt={movie.title} /> : movie.original_language}</li>
                         <li>{movie.vote_average}</li>
                     </ul>
                 ))
